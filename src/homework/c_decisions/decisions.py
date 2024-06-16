1e02f60f820191e913b6291cc2df796891b5b54a
matthewrz1 <matthewrz96@gmail.com>
2024-06-16 01:54:22 +0000

    Homework 3 files

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,21 @@
-#
+#Homework 3 decisions functions file
+
+def get_options_ratio(option,total):
+    ratio = int(option)/int(total)
+    return ratio
+
+def get_faculty_rating(ratio):
+    if ratio >= .9 and ratio < 1:
+        return "Excellent"
+    
+    elif ratio >= .8 and ratio < .9:
+        return "Very Good"
+        
+    elif ratio >= .7 and ratio < .8:
+        return "Good"
+    
+    elif ratio >= .6 and ratio <.7:
+        return "Needs Improvement"
+    
+    elif ratio >= 0 and ratio <= .5999999999:
+            return "Unacceptable"
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,13 @@
-#
+#Main Homework 3 py file
+
+import decisions
+
+option = float(input("Enter an option: "))
+
+total = int(input("Enter a total: "))
+
+ratio = decisions.get_options_ratio(option,total)
+
+print(decisions.get_faculty_rating(ratio))
+
+
