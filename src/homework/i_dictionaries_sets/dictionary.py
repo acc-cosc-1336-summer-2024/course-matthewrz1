d8d02dbb3dc58a4d09a887c2fc42e86400d582f6
matthewrz1 <matthewrz96@gmail.com>
2024-07-14 03:40:17 +0000

    Homework 6 files, 4 files

--- src/homework/i_dictionaries_sets/dictionary.py
+++ src/homework/i_dictionaries_sets/dictionary.py
@@ -1,1 +1,43 @@
-#
+#Homework 6 functions
+
+def get_p_distance(list1, list2):
+
+    #get distance function of 2 lists
+
+    y = 0
+
+    for x in range(0,len(list1)):
+        if list1[x] != list2[x]:
+            y += 1
+    
+    p_distance = y/(len(list1))
+
+    return p_distance
+
+
+def get_p_distance_matrix(list1):
+
+    pre_distance_matrix = []
+
+    l1 = [i for i in range(0, len(list1))]
+
+    j = 0
+    for i in range(0, len(list1)):
+        for j in range(0, len(list1)):
+            x = get_p_distance(list1[i],list1[j])
+            pre_distance_matrix.append(x)
+
+    matrix = []
+    k = 0
+
+    for i in range(0,len(l1)):
+        pre_matrix = []
+        for j in range(0,len(l1)):
+            pre_matrix.append(pre_distance_matrix[k])
+            k += 1
+        
+        matrix.append(pre_matrix)
+
+    return matrix
+
+
--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -1,1 +1,68 @@
-#
+#homework 6 main file with menu
+
+import dictionary
+
+def menu():
+
+    print("Homework 6 menu, please choose an option below")
+    print("1-Get p distance matrix")
+    print("2-Exit")
+
+    selection = int(input("Please choose a menu item above: "))
+    x = True
+    Ask_Exit = "N"
+
+    if selection == 1:
+
+        row_count = int(input("You have chosen get p distance matrix, enter how many rows list will have: "))
+        q_count = 0
+        list1 = []
+
+        while q_count < row_count:
+            sub_list = input("Enter items to be added to the list without a space or commas: ")
+            int_list =[]
+            for g in range(0,len(sub_list)):
+                int_list.append(sub_list[g])
+            list1.append(int_list)
+            q_count += 1
+                         
+        while x == True:
+            if len(list1) > 0:
+                print("p distance matrix of", list1 , " is: ", dictionary.get_p_distance_matrix(list1))
+                Ask_Exit = input("Would you like to exit? Y or N: ").upper()
+                if Ask_Exit == "Y":
+                    print("You have exit the menu")
+                    exit()
+                elif Ask_Exit == "N":
+                    print("Staying in p distance matrix menu option")
+                    row_count = int(input("You have chosen to stay in p distance matrix, enter how many rows list will have: "))
+                    q_count = 0
+                    list1 = []
+                    while q_count < row_count:
+                        sub_list = input("Enter items to be added to the list without a space or commas: ")
+                        int_list =[]
+                        for g in range(0,len(sub_list)):
+                            int_list.append(sub_list[g])
+                        list1.append(int_list)
+                        q_count += 1
+
+            elif len(list1) < 1:
+                list1 = input("List is empty, enter a list: ")
+    
+
+    elif selection == 2:
+
+        while x == True:
+            Ask_Exit = input("Would you like to continue? Y or N: ").upper()
+            if Ask_Exit == "N":
+                print("You have exit the menu")
+                exit()
+            elif Ask_Exit == "Y":
+                menu()
+    
+    else:
+        print("Menu option not recognized, please input a number from 1 to 2")
+        menu()
+
+
+menu()
